cff68c6fd613c1029c130cd917e89bd8437b3c54
张哲 <jefferylong@163.com>
2024-07-27 23:32:44 +0800

    feat: add webview

---
+++ components/Webview/index.tsx
@@ -0,0 +1,22 @@
+import Constants from "expo-constants";
+import { StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function AppWebView() {
+  return (
+    <WebView
+      source={{ uri: "https://reactnative.dev/" }}
+      onError={(e) => {
+        console.log("err__", e);
+      }}
+      style={{ flex: 1, backgroundColor: "orange" }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
